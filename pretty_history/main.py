8cf9db2af6c32ce280c46b274ac1a1f522f9c869
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2022-09-10 13:39:05 +0100

    add new param for controlling output dir
    closes #5

--- pretty_history/main.py
+++ pretty_history/main.py
@@ -11,19 +11,24 @@ def main():
         description="Generate a pretty view of your browser history from a JSON export."
     )
     parser.add_argument(
-        "-j",
-        "--json",
+        dest="json_file",
         action="store",
         type=pathlib.Path,
-        required=True,
-        metavar="~/Documents/history.json",
-        dest="json_file",
         help="The absolute or relative path to the JSON file containing your browser history. The schema of the JSON "
-        "file should match the output from seanbreckenridge/browserexport",
+        "file should match the output from 'seanbreckenridge/browserexport'.",
+    )
+    parser.add_argument(
+        "-o",
+        dest="destination_folder",
+        action="store",
+        type=pathlib.Path,
+        required=False,
+        metavar="./folder",
+        help="The absolute or relative path to a folder that you want to send the generated output to."
     )
     args: Namespace = parser.parse_args()
 
-    prettyhist.prettify(history_json=args.json_file)
+    prettyhist.prettify(history_json=args.json_file, dumping_folder=args.destination_folder)
 
 
 if __name__ == "__main__":
